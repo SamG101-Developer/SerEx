0bc6186a8520c9229f076ec5e87fdc081fcee062
SamG101-Developer <samuelgardner101@gmail.com>
2025-11-01 04:43:32 +0000

    Export all

--- src/SerEx/serialize.cpp
+++ src/SerEx/serialize.cpp
@@ -3,4 +3,7 @@ export module serex.serialize;
 export import serex.archive;
 export import serex.boolean;
 export import serex.numbers;
+export import serex.polymorphic;
 export import serex.string;
+export import serex.tuple;
+export import serex.vector;
