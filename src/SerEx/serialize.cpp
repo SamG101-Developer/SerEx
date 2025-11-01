8e36a286a9dc13c062e5adbf687f08871fe63449
SamG101-Developer <samuelgardner101@gmail.com>
2025-11-01 05:40:59 +0000

    Support for `unique_ptr`

--- src/SerEx/serialize.cpp
+++ src/SerEx/serialize.cpp
@@ -4,6 +4,7 @@ export import serex.archive;
 export import serex.boolean;
 export import serex.numbers;
 export import serex.polymorphic;
+export import serex.pointers;
 export import serex.string;
 export import serex.tuple;
 export import serex.vector;
