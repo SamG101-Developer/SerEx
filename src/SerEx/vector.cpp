03e8ef34fea7e5e7c1807f8b9fd6268b71c99054
SamG101-Developer <samuelgardner101@gmail.com>
2025-11-02 19:53:07 +0000

    Vector optimization (way less space)

--- src/SerEx/vector.cpp
+++ src/SerEx/vector.cpp
@@ -4,6 +4,34 @@ import serex.archive;
 
 
 template <typename T, typename A>
+    requires std::is_trivially_copyable_v<T>
+struct serex::Serializer<std::vector<T, A>> {
+    static auto save(std::vector<T, A> const &obj) -> std::string {
+        auto stream = std::string();
+        const auto element_size = sizeof(T);
+        stream.append(reinterpret_cast<const char*>(&element_size), sizeof(std::size_t));
+        for (const auto &item : obj) {
+            auto partial = Serializer<T>::save(item);
+            stream.append(partial);
+        }
+        return stream;
+    }
+
+    static auto load(const std::string &s) -> std::vector<T, A> {
+        auto vec = std::vector<T, A>{};
+        std::size_t element_size;
+        std::memcpy(&element_size, s.data(), sizeof(std::size_t));
+        for (auto i = sizeof(std::size_t); i < s.size(); i += element_size) {
+            auto item_data = s.substr(i, element_size);
+            auto item = Serializer<T>::load(item_data);
+            vec.push_back(std::move(item));
+        }
+        return vec;
+    }
+};
+
+
+template <typename T, typename A>
 struct serex::Serializer<std::vector<T, A>> {
     static auto save(std::vector<T, A> const &obj) -> std::string {
         auto stream = std::string();
@@ -25,7 +53,7 @@ struct serex::Serializer<std::vector<T, A>> {
             auto item_data = s.substr(i, item_size);
             auto item = Serializer<T>::load(item_data);
             vec.push_back(std::move(item));
-            i += item_size - sizeof(T);
+            i += item_size;
         }
         return vec;
     }
