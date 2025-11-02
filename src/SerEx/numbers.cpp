e7fc4eb5acd8fa220691c7b21bdb5bcf7015d788
SamG101-Developer <samuelgardner101@gmail.com>
2025-11-02 20:43:29 +0000

    Fix numeric conversions

--- src/SerEx/numbers.cpp
+++ src/SerEx/numbers.cpp
@@ -3,48 +3,23 @@ import std;
 import serex.archive;
 
 
-template <typename T> requires std::integral<T> or std::floating_point<T>
+/**
+ * Because numbers of the same type don't always have the same length ("1" and "10" for example), numbers are copied
+ * into buffers with fixed lengths. For "std::uint8_t", this is an 1-byte buffer.
+ * @tparam T
+ */
+template <typename T>
+requires std::integral<T> or std::floating_point<T>
 struct serex::Serializer<T> {
     static auto save(T obj) -> std::string {
-        return std::to_string(obj);
+        auto buffer = std::string(sizeof(T), '\0');
+        std::memcpy(buffer.data(), &obj, sizeof(T));
+        return buffer;
     }
 
     static auto load(const std::string &s) -> T {
-        if constexpr (std::same_as<T, std::uint8_t>) {
-            return static_cast<T>(std::stoi(s));
-        }
-        else if constexpr (std::same_as<T, std::int8_t>) {
-            return static_cast<T>(std::stoul(s));
-        }
-        else if constexpr (std::same_as<T, std::int16_t>) {
-            return static_cast<T>(std::stoi(s));
-        }
-        else if constexpr (std::same_as<T, std::uint16_t>) {
-            return static_cast<T>(std::stoul(s));
-        }
-        else if constexpr (std::same_as<T, std::int32_t>) {
-            return static_cast<T>(std::stoi(s));
-        }
-        else if constexpr (std::same_as<T, std::uint32_t>) {
-            return static_cast<T>(std::stoul(s));
-        }
-        else if constexpr (std::same_as<T, std::int64_t>) {
-            return static_cast<T>(std::stoll(s));
-        }
-        else if constexpr (std::same_as<T, std::uint64_t>) {
-            return static_cast<T>(std::stoull(s));
-        }
-        else if constexpr (std::same_as<T, float>) {
-            return std::stof(s);
-        }
-        else if constexpr (std::same_as<T, double>) {
-            return std::stod(s);
-        }
-        else if constexpr (std::same_as<T, long double>) {
-            return std::stold(s);
-        }
-        else {
-            std::unreachable();
-        }
+        T obj;
+        std::memcpy(&obj, s.data(), sizeof(T));
+        return obj;
     }
 };
